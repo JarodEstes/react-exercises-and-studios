68a0e1188f9d56b355d1855f2ac0b372400a5ce7
JarodEstes <estesjarod48@gmail.com>
2024-03-11 15:47:33 -0500

    React exercises

--- part1/exercises/src/components/BookList.jsx
+++ part1/exercises/src/components/BookList.jsx
@@ -1,15 +1,16 @@
 export default function BookList() {
-   let pageTitle = "shrug";
-   let book1 = "google.com";
-   let book2 = "";
-   let book3 = "";
+   let pageTitle = "Best Books!";
+   let book1 = "https://m.media-amazon.com/images/I/61pDNU9qEGL._SL1360_.jpg";
+   let book2 = "https://m.media-amazon.com/images/I/613W6rlIiKL._SL1500_.jpg";
+   let book3 = "https://m.media-amazon.com/images/I/71ygzPIKlHL._SL1500_.jpg";
 
    return (
       <div>
          <h3>{pageTitle}</h3>
-         <img src={book1} alt="Nothing here!" />
-         <img src={book2} alt="Nothing here!" />
-         <img src={book3} alt="Nothing here!" />
+         <img src={book1} alt="Cant Hurt Me - by David Goggins!" />
+         <img src={book2} alt="Michael Jordan The Life - by Roland Lazenby!" />
+         <img src={book3} alt="Why I stand - by Jonathan Issac!" />
       </div>      
    );
 }
+https://m.media-amazon.com/images/I/613W6rlIiKL._SL1500_.jpg
