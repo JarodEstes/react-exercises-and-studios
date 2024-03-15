487667a61222a3f06ea81815ffa7faabf6be1e95
JarodEstes <estesjarod48@gmail.com>
2024-03-14 20:35:29 -0500

    react studio part 2

--- part2/studio/src/App.jsx
+++ part2/studio/src/App.jsx
@@ -1,20 +1,22 @@
 import "./App.css";
 import RateARecipe from "./components/RateARecipe";
+import ClickedButton from "./components/ClickedButton";
 import Button from "./components/Button";
 import AuthorInfo from "./components/AuthorInfo";
 import IngredientList from "./components/Ingredients";
 import RecipeImage from "./components/RecipeImage";
 import RecipeName from "./components/RecipeName";
+import React from "react";
 
 export default function App() {
   return (
     <>
-      <RecipeImage />
-      <RecipeName />
+      {/* <RecipeImage />
+      <RecipeName /> */}
       <RateARecipe rating={4} />
-      <Button saveButton={true} />
-      <AuthorInfo />
-      <IngredientList />
+      <ClickedButton saveButton={false} />
+      {/* <AuthorInfo />}
+      {<IngredientList /> */}
     </>
   );
 }
--- part2/studio/src/components/Ingredients.jsx
+++ part2/studio/src/components/Ingredients.jsx
@@ -1,5 +1,20 @@
+import recipedata from ""
+
 function IngredientList() {
-   return;
+   return (
+    <div>
+      <h3>Ingrediends</h3>
+      {recipedata.map((data) =>{
+        return (
+          <div key={data.name}>
+            {data.ingrediends.map((ingredient, index) => {
+              return <li key={index}>{ingredient}</li>
+            })}
+          </div>
+        )
+          } )}
+    </div>
+   )
  }
  
  export default IngredientList;
